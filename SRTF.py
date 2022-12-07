957685607f8d21b5270d9e8fd8057c2953b2cbd0
Mann202 <95089843+Mann202@users.noreply.github.com>
2022-12-07 11:10:26 +0700

    Update SRTF.py

--- SRTF.py
+++ SRTF.py
@@ -5,28 +5,27 @@
 # File: task2-SRTF.py                    #    
 ##########################################
 
-n = int(input('Enter total number of process: '))   # Nhập số lượng process cần được tính toán
+n = int(input('Enter total number of process: '))  
 
-# Tạo mảng với n Process, BurstTime và ArrivalTime
+#Tạo mảng theo n
 Process = [0] * (n+1)
 ArrivalTime = [0] * (n+1)
 BurstTime = [0] * (n+1)
 StartTime = [0] * (n+1)
 resTime = [0] * (n + 1)
 
-# Tạo mảng cho Finish Time, TurnAround Time và Waiting Time
 finishTime = [0] * (n + 1)
 turnTime = [0] * (n + 1)
 waitTime = [0] * (n + 1)
 
-# Nhập vào BurstTime và ArrivalTime theo thứ tự n
-for i in range(n):  # Chạy vòng lặp
+#Nhập vào mảng
+for i in range(n):  
     print("\n")
     BurstTime[i] = int(input('Enter the burst time for process {} : '.format(i + 1)))
     ArrivalTime[i] = int(input('Enter the arrival time for process {} : '.format(i + 1))) 
-    Process[i] = [BurstTime[i], ArrivalTime[i], i, StartTime[i]]  # Cho BurstTime, ArrivalTimes và thứ tự Process vào một mảng Process
+    Process[i] = [BurstTime[i], ArrivalTime[i], i, StartTime[i]]  
 
-# Xóa phần tử 0 xuất hiện khi tạo mảng
+#Xóa phần tử 0 
 Process.pop(-1) 
 finishTime.pop(-1)
 waitTime.pop(-1)
@@ -36,34 +35,49 @@ ArrivalTime.pop(-1)
 StartTime.pop(-1)
 resTime.pop(-1)
 i = 0
-list = []  # Tạo mảng chứa những Process đã được xử lí
+list = []  
 
-for i in range(0, sum(BurstTime)):  #  Chạy vòng lặp trong khoảng từ 0 đến tổng thời gian BurstTime của các tiến trình
+#Hoán đổi phần tử có arrival time = 0 lên đầu mảng
+tag = Process[0]
+
+for i in Process:
+    if i[1] == 0:
+        tag = i
+        index = Process.index(tag) 
+
+temp = Process[0]
+Process[0] = tag
+Process[index] = temp
+
+print(Process)
+
+#Xử lí Process
+for i in range(0, sum(BurstTime)):
 	l = [j for j in Process  if j[1] <= i]
-	l.sort(reverse= False ,key=lambda x: x[0]) # Sắp xếp lại Burst Time của các process theo thứ tự tăng dần 
+	l.sort(reverse= False ,key=lambda x: x[0]) 
 	Process[Process.index(l[0])][0] -= 1 
 	if(Process[Process.index(l[0])][3] == 0):
 		Process[Process.index(l[0])][3] = i
 	for k in Process:
-		if k[0] == 0:   # Nếu Burst Time của 1 Process bằng 0 thì 
+		if k[0] == 0:   
 			t = Process.pop(Process.index(k))
-			list.append([k, i + 1])	# Thêm process [thông tin process, thời gian hoàn thành] đã được xử lí vào mảng process đã được xử lí (list)
-print(list)
-for i in list:
-    finishTime[i[0][2]] = i[1] # Thêm thời gian hoàn thành (Finish time) vào mảng finishTime theo thứ tự i của Process4
-    StartTime[i[0][2]] = i[0][3]
-    StartTime[0] = 0
+			list.append([k, i + 1])	
 
-for i in range(len(finishTime)):    # Chạy vòng lặp theo số lượng phần tử có trong mảng finishTime
-    # Tính toán TurnAround Time và Waiting Time
+for i in list:
+    finishTime[i[0][2]] = i[1] 
+    StartTime[i[0][2]] = i[0][3]
+
+#Tính toán 
+for i in range(len(finishTime)):    
 	turnTime[i] = finishTime[i] - ArrivalTime[i]    
 	waitTime[i] = turnTime[i] - BurstTime[i]
 	resTime[i] = StartTime[i] - ArrivalTime[i]
 
+#Xuất kết quả
 print("\n")
 print('ID\tBurstTime\tArrivalTime\tStartTime\tFinishTime\tResponseTime\tTurnTime\tWaitTime')
-for i in range(len(finishTime)):    # Chạy vòng lặp với độ dài của mảng Finish Time
-	print("{}\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\n".format(i, BurstTime[i], ArrivalTime[i],StartTime[i], finishTime[i], resTime[i], turnTime[i], waitTime[i]))   #In ra màn hình kết quả
-print('Average Waiting Time = ', sum(waitTime)/len(waitTime))   # Tính toán và in ra màn hình kết quả của thời gian đợi trung bình
-print('Average Turnaround Time = ', round(sum(turnTime)/len(turnTime), 3))    # Tính toán và in ra màn hình kết quả của thời gian hoàn thành trung bình
+for i in range(len(finishTime)):   
+	print("{}\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\t\t{}\n".format(i, BurstTime[i], ArrivalTime[i],StartTime[i], finishTime[i], resTime[i], turnTime[i], waitTime[i]))   
+print('Average Waiting Time = ', sum(waitTime)/len(waitTime))  
+print('Average Turnaround Time = ', round(sum(turnTime)/len(turnTime), 3))    
 print('Average Response Time = ', round(sum(resTime)/len(resTime), 3))
